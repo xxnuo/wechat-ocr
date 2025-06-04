77aa6199090be727a39770d8f83740e4089f61c9
xxnuo <54252779+xxnuo@users.noreply.github.com>
2025-06-04 22:13:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,16 @@
 #! /bin/bash
 
 # need: uv ccache gcc make cmake
+# usage (python 3.12): chmod +x ./build.sh && ./build.sh 3.12
 
 export CC="ccache gcc"
 export CXX="ccache g++"
 
-uv venv -p 3.12
+uv venv -p $1
 source .venv/bin/activate
 mkdir build
 cd build 
 cmake ..
 make -j10
 cd ..
-mv build build_py3.12
-
-uv venv -p 3.10
-source .venv/bin/activate
-mkdir build
-cd build 
-cmake ..
-make -j10
-mv build build_py3.10
+mv build build_py$1
